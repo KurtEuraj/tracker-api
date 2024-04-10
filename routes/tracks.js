d14215322f3103c62d9fb5aafad0bb67cabf323b
Kurt Euraj <kurteurajdev@gmail.com>
2024-04-10 13:37:16 -0400

    Fix post tracks route

--- routes/tracks.js
+++ routes/tracks.js
@@ -3,7 +3,7 @@ const router = express.Router()
 require("dotenv").config()
 const PORT = process.env.PORT || 8080
 const request = require("request")
-const axios = require ("axios")
+const axios = require("axios")
 const OpenAI = require("openai")
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
@@ -24,18 +24,14 @@ const getAccessToken = async () => {
         headers: {
             'Authorization': 'Basic ' + (new Buffer.from(process.env.OPENAI_CLIENT_ID + ':' + process.env.OPENAI_CLIENT_SECRET).toString('base64'))
         },
-        form: {
-            grant_type: 'client_credentials'
-        },
-        json: true
+        data: "grant_type=client_credentials",
     };
 
-    request.post(authOptions, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            accessToken = body.access_token
-            tokenExpiry = Date.now() + 3600000
-        }
-    });
+    const response = await axios.post(authOptions.url, authOptions.data, { headers: authOptions.headers });
+    if (response.status === 200) {
+        accessToken = response.data.access_token;
+        tokenExpiry = Date.now() + 3600000;
+    }
 }
 
 router.post("/", async (req, res) => {
@@ -46,37 +42,80 @@ router.post("/", async (req, res) => {
     const songs = []
 
     const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
+        model: "gpt-4-turbo",
         messages: [
             { "role": "system", "content": "You are very knowledgeable about music" },
             { "role": "system", "content": "You reply only with the song name and artists" },
+            { "role": "system", "content": "You don't reply with the same song that is inputted" },
+            { "role": "system", "content": "The songs you give back must be available on Spotify" },
             { "role": "system", "content": `You reply only with this format ${format}` },
             { "role": "user", "content": "I am going to ask for song recommendations based on my favorite song" },
             { "role": "user", "content": `Give me 6 songs like ${req.body.song}` }]
     });
 
     const gptSongs = completion.choices[0].message.content
+    console.log(`GPT returned songs: ${gptSongs}`)
     const splitSongs = gptSongs.split('\n')
 
     splitSongs.forEach((song) => {
         const parts = song.split('" by ');
         const songNameStr = parts[0].replace(/^\d+\.\s+\"/, '');
-        const songName = songNameStr.replaceAll(" ", "%25")
-        const artist = parts[1].replaceAll(" ", "%25");
+        const songName = songNameStr.replaceAll(" ", "+")
+        const artist = parts[1].replaceAll(" ", "+");
         songs.push({ song: songName, artist: artist });
     });
+    console.log(songs)
 
     const allSongsData = await Promise.all(songs.map(async (songItem) => {
-        const response = await axios.get(`https://api.spotify.com/v1/search?q=track3A${songItem.song}artist%3A${songItem.artist}&type=track&limit=1`,
+        const response = await axios.get(`https://api.spotify.com/v1/search?q=track3A${songItem.song}artist%3A${songItem.artist}&type=track`,
             {
                 headers: {
                     'Authorization': `Bearer ${accessToken}`,
                 }
             })
-        return response.data.tracks.items
+        for (const track of response.data.tracks.items) {
+            if (track.name.toLowerCase().includes(songItem.song.toLowerCase().replaceAll("+", " ")) && (songItem.artist.toLowerCase().replaceAll("+", " ").includes(track.artists[0].name.toLowerCase()))) {
+                return track.id
+            }
+        }
     }))
+    const selectedSongs = allSongsData.filter((song) => song !== undefined)
+    res.json(selectedSongs)
+})
+
+router.post("/search", async (req, res) => {
+    if (tokenExpiry <= Date.now()) {
+        await getAccessToken()
+    }
+
+    const response = await axios.get(`https://api.spotify.com/v1/search?q=track3A${req.body.search}&type=track&limit=10`,
+        {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+            }
+        })
+    const allSongs = response.data.tracks.items
+    const allSongsData = []
+    allSongs.forEach((song) => {
+        const songName = song.name
+        let artistsArr = []
+        let artists = ""
+        if (song.artists.length > 1) {
+            song.artists.forEach((artist) => {
+                artistsArr.push(artist.name)
+            })
+            artists = artistsArr.join(", ")
+        }
+        else {
+            artists = song.artists[0].name
+        }
+        const songData = {
+            song: songName,
+            artists: artists
+        }
+        allSongsData.push(songData)
+    })
     res.json(allSongsData)
-    console.log(`Access Token: ${accessToken}`)
 })
 
 module.exports = router
